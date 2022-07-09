d3f07cfd957c698e6bc16e6c162565d5e81b9dc2
Jacques kibamba <jacqueskibamba@gmail.com>
2022-07-09 16:26:51 +0300

    Defining a schema and preloading data

--- src/main/java/ru/springinaction/first/tacocloud/TacoCloudApplication.java
+++ src/main/java/ru/springinaction/first/tacocloud/TacoCloudApplication.java
@@ -1,7 +1,9 @@
 package ru.springinaction.first.tacocloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
